6081a3d5925dccb60904dca88da4f8818fc54999
Mitul Shah <mitulxshah@gmail.com>
2017-08-16 19:30:20 -0400

    both vanilla js and jquery options, form validation works

--- script.js
+++ script.js
@@ -1,10 +1,30 @@
 var wrong = "Please enter a valid email address";
 var correct = "Thanks! We'll be in touch.";
+var email = document.getElementById('email'), regex = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
 
-function validate() {
+// jQuery; avoids redirect.
+
+$(document).ready(function () {
+	'use strict';
+    $("#submit").click(function (event) {
+        event.preventDefault();
+		if (!regex.test(email.value)) {
+			$('#correct').html("");
+			$('#wrong').html(wrong);
+		} else {
+			$('#wrong').html("");
+			$('#correct').html(correct);
+		}
+		$("form").submit();
+    });
+});
+
+// Vanilla Javascript
+
+/*function validate() {
 	'use strict';
 	var email = document.getElementById('email'), regex = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
-	if (regex.test(email.value)) {		
+	if (regex.test(email.value)) {
 		document.getElementById('wrong').innerHTML = "";
 		document.getElementById('correct').innerHTML = correct;
 		return true;
@@ -12,7 +32,9 @@ function validate() {
 		document.getElementById('correct').innerHTML = "";
 		document.getElementById('wrong').innerHTML = wrong;
 		email.focus;
-		return false;	
+		return false;
 	}
-	console.log('tex');
 }
+
+var sub = document.querySelector("#submit");
+sub.addEventListener("click", validate);*/
