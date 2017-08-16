cc82afba75dcb000b456e386c75bda6266ec5945
Mitul Shah <mitulxshah@gmail.com>
2017-08-15 23:45:46 -0400

    validation complete, will improve if possible

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-alert('connect');
+var wrong = "Please enter a valid email address";
+var correct = "Thanks! We'll be in touch.";
+
+function validate() {
+	'use strict';
+	var email = document.getElementById('email'), regex = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+	if (regex.test(email.value)) {		
+		document.getElementById('wrong').innerHTML = "";
+		document.getElementById('correct').innerHTML = correct;
+		return true;
+	} else {
+		document.getElementById('correct').innerHTML = "";
+		document.getElementById('wrong').innerHTML = wrong;
+		email.focus;
+		return false;	
+	}
+	console.log('tex');
+}
